71c3b5d8e0057742e75963a16c6b4f846cbb50c4
itsGBN <nem8887@nyu.edu>
2024-02-04 18:48:21 -0500

    V2 IDK

--- IntrotoProgDay2HW/Assets/Scripts/CardManager.cs
+++ IntrotoProgDay2HW/Assets/Scripts/CardManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Drawing;
 using UnityEngine;
 
@@ -8,40 +9,52 @@ public class CardManager : MonoBehaviour
     [SerializeField] Transform rockTarget;
     [SerializeField] Transform paperTarget;
     [SerializeField] Transform scissorsTarget;
-    [SerializeField] Transform point;
-    bool allowedToMove = false;
+
+    [SerializeField] GameObject rock;
+    [SerializeField] GameObject paper;
+    [SerializeField] GameObject scissors;
+
+    [SerializeField] GameObject gameManager;
+
+    Holay holay;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        holay = gameManager.GetComponent<Holay>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (transform.position != point.transform.position)
-        {
-            allowedToMove = true;
-        }
-        else
-        {
-            allowedToMove = false;
-        }
+        //print(holay.move);
+        //print(GameObject.FindGameObjectsWithTag("0") != null);
 
-        if (allowedToMove)
+        //if (Holay.move)
+        //{
+        //    switch (gameObject.tag)
+        //    {
+        //        case "0":
+        //            transform.position = Vector3.Lerp(transform.position, rockTarget.transform.position, Time.deltaTime * 2);
+        //            break;
+        //        case "1":
+        //            transform.position = Vector3.Lerp(transform.position, paperTarget.transform.position, Time.deltaTime * 2);
+        //            break;
+        //        case "2":
+        //            transform.position = Vector3.Lerp(transform.position, scissorsTarget.transform.position, Time.deltaTime * 2);
+        //            break;
+
+        //    }
+        //    Holay.move = false;
+        //}
+
+        if (holay.move)
         {
-            switch (gameObject.tag)
+            if(GameObject.FindGameObjectsWithTag("0") != null)
             {
-                case "0":
-                    transform.position = Vector3.Lerp(transform.position, point.transform.position, Time.deltaTime * 2);
-                    break;
-                case "1":
-                    transform.position = Vector3.Lerp(transform.position, point.transform.position, Time.deltaTime * 2);
-                    break;
-                case "2":
-                    transform.position = Vector3.Lerp(transform.position, point.transform.position, Time.deltaTime * 2);
-                    break;
-
+                rock.transform.position = Vector3.Lerp(rock.transform.position, rockTarget.transform.position, Time.deltaTime * 2);
+                print("HI");
             }
         }
     }
--- IntrotoProgDay2HW/Assets/Scripts/Holay.cs
+++ IntrotoProgDay2HW/Assets/Scripts/Holay.cs
@@ -3,25 +3,34 @@ using System.Collections.Generic;
 using System.Drawing;
 using UnityEngine;
 
+public enum GameStatus { Start, Ready, End };
 public class Holay : MonoBehaviour
 {
     [SerializeField] GameObject[] cards;
-
+    public bool move = false;
+    public static GameStatus gameStatus;
 
     // Start is called before the first frame update
     void Start()
     {
+        gameStatus = GameStatus.Start;
     }
 
     // Update is called once per frame
     void Update()
     {
-        for(int i = 0; i < cards.Length; i++)
+
+        if(gameStatus == GameStatus.Start)
         {
-            if (GameObject.FindGameObjectsWithTag(i.ToString()).Length < 1)
+            for (int i = 0; i < cards.Length; i++)
             {
-                Instantiate(cards[i], new Vector3(67f, 13f, 0f), Quaternion.identity);
+                if (GameObject.FindGameObjectsWithTag(i.ToString()).Length < 1)
+                {
+                    Instantiate(cards[i], new Vector3(67f, 13f, 0f), Quaternion.identity);
+                }
+                move = true;
             }
+
         }
     }
 }
